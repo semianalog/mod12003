d36422b305786af1e26f999d1cb61abe78d047b1
Chris Pavlina <cpavlin1@binghamton.edu>
2015-10-31 17:15:20 -0400

    Remove old firmware

--- fw/pins.c
+++
@@ -1,8 +0,0 @@
-#include "pins.h"
-
-#define X(port, pin, name, dir, dval, drive) {&PORT##port, (1 << pin)},
-const struct PINS_pin_t PINS_pins[] =
-{
-    SYSTEM_CONF_PINS
-};
-#undef X
--- fw/psu.c
+++
@@ -1,8 +0,0 @@
-#include "config.h"
-#include "psu.h"
-#include "hardware.h"
-
-void psu_control_step(void)
-{
-    i2cdac_set(0);
-}
